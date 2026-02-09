f309cedd819652f29213c0855a1515eaff5e82f9
DumbCaesar <dumbcaesar94@gmail.com>
2026-02-09 13:04:26 +0100

    bfs implementation

---
+++ Algoritmer/Edge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritmer
+{
+    public class Edge<T>
+    {
+        public Node<T> From { get; set; }
+        public Node<T> To { get; set; }
+
+        public Edge(Node<T> from, Node<T> to)
+        {
+            From = from;
+            To = to;
+        }
+    }
+}
