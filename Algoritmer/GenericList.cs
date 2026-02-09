878c22db79c948f740b2efc8e13ec85d6aef8574
DumbCaesar <dumbcaesar94@gmail.com>
2026-02-09 09:01:44 +0100

    generic list implemented

---
+++ Algoritmer/GenericList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritmer
+{
+    public class GenericList<T>
+    {
+        T[] TheList { get; set; } = new T[] {};
+
+        public GenericList(T[] values) 
+        {
+            TheList = values;
+        }
+
+        public void Add(T value)
+        {
+            int count = TheList.Length;
+            T[] tmpList = new T[count + 1];
+            TheList.CopyTo(tmpList, 0);
+            tmpList[count] = value;
+            TheList = tmpList;
+        }
+
+        public int Count()
+        {
+            return TheList.Length;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in TheList)
+            {
+                yield return item;
+            }
+        }
+    }
+}
--- Algoritmer/Program.cs
+++ Algoritmer/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            
+            GenericList<int> genericList = new GenericList<int>();
+            genericList.Add(1);
+            genericList.Add(2);
+            genericList.Add(3);
+            genericList.Add(4);
+            genericList.Add(5);
+            genericList.Add(6);
+            genericList.Add(7);
+            genericList.Add(8);
+            genericList.Add(9);
+            genericList.Add(10);
+
+            genericList.Add(11);
+
+            foreach (var i in genericList)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
