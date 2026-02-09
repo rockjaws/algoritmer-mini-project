9f420ae407435899075b608c932ab4906a252fc7
Nicolaj Hansen <nicolaj_95@outlook.com>
2026-02-09 09:20:33 +0100

    please accept my algorithms

---
+++ Algoritmer/BubbleSortAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿namespace Algoritmer;
+
+public class BubbleSortAlgorithm<T> where T : IComparable<T>
+{
+  public T[] BubbleSort(T[] arr)
+  {
+    for (int i = 1; i < arr.Length; i++)
+    {
+      T val = arr[i];
+      int pointer = i;
+
+      while (pointer > 0 && val.CompareTo(arr[pointer - 1]) < 0)
+      {
+        arr[pointer] = arr[pointer - 1];
+        pointer--;
+      }
+      arr[pointer] = val;
+    }
+    return arr;
+  }
+}
---
+++ Algoritmer/QuickSortAlgorithm.cs
@@ -0,0 +1,41 @@
+﻿namespace Algoritmer;
+
+public class QuickSortAlgorithm<T> where T : IComparable<T>
+{
+  public T[] QuickSort(T[] arr)
+  {
+    if (arr.Length <= 1) return arr;
+
+    int pivot = arr[0];
+    int beforeSize = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+      if (arr[i] < pivot) beforeSize++;
+    }
+    int[] before = new int[beforeSize];
+    int beforeCounter = 0;
+
+    int[] after = new int[arr.Length - beforeSize - 1];
+    int afterCounter = 0;
+
+    for (int i = 1; i < arr.Length; i++)
+    {
+      if (arr[i] < pivot)
+      {
+        before[beforeCounter] = arr[i];
+        beforeCounter++;
+      }
+      else
+      {
+        after[afterCounter] = arr[i];
+        afterCounter++;
+      }
+    }
+
+    int[] tmpArr = new int[arr.Length];
+    QuickSort(before).CopyTo(tmpArr, 0);
+    tmpArr[before.Length] = pivot;
+    QuickSort(after).CopyTo(tmpArr, before.Length + 1);
+    return tmpArr;
+  }
+}
