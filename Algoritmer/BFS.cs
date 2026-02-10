c55465542b3ce7caf7669e8f5638aca68b0ac5cd
Nicolaj Hansen <nicolaj_95@outlook.com>
2026-02-10 09:13:52 +0100

    pretty printing

--- Algoritmer/BFS.cs
+++ Algoritmer/BFS.cs
@@ -5,74 +5,78 @@ using System.Text;
 
 namespace Algoritmer
 {
-    public class BFS<T>
+  public class BFS<T>
+  {
+    public int Comparisons { get; private set; } = 0;
+
+    public void BFSRoute(Graph<T> graph, Node<T> source, Node<T> destination)
     {
-        public int Comparisons { get; private set; } = 0;
+      Comparisons = 0;
 
-        public void BFSRoute(Graph<T> graph, Node<T> source, Node<T> destination)
+      List<Node<T>> exitToSource = new List<Node<T>>();
+      List<Node<T>> marked = new List<Node<T>>();
+      Dictionary<Node<T>, Node<T>> parent = new Dictionary<Node<T>, Node<T>>();
+      Queue<Node<T>> queue = new Queue<Node<T>>();
+
+      queue.Enqueue(source);
+      parent[source] = null;
+
+      Console.WriteLine();
+      Console.WriteLine("Starting BFS...");
+      Console.Write("Order of nodes visited: ");
+      while (queue.Count > 0)
+      {
+        Node<T> node = queue.Dequeue();
+        Console.Write(node.Data);
+
+        if (marked.Contains(node))
+          continue;
+        marked.Add(node);
+
+        if (node.Equals(destination))
         {
-            Comparisons = 0;
-
-            List<Node<T>> exitToSource = new List<Node<T>>();
-            List<Node<T>> marked = new List<Node<T>>();
-            Dictionary<Node<T>, Node<T>> parent = new Dictionary<Node<T>, Node<T>>();
-            Queue<Node<T>> queue = new Queue<Node<T>>();
-
-            queue.Enqueue(source);
-            parent[source] = null;
-
-            Console.WriteLine();
-            Console.WriteLine("Starting BFS...");
-            while (queue.Count > 0)
-            {
-                Node<T> node = queue.Dequeue();
-                Console.WriteLine(node.Data);
-
-                if (marked.Contains(node))
-                    continue;
-
-                if (node.Equals(destination))
-                {
-                    Console.WriteLine();
-                    Console.WriteLine($"Desitnation found {node.Data}");
-                    exitToSource.Add(node);
-                    break;
-                }
-                foreach (var edge in node.Edges)
-                {
-                    if (!parent.ContainsKey(edge.To))
-                    {
-                        parent[edge.To] = node;
-                        queue.Enqueue(edge.To);
-                    }
-                }
-                marked.Add(node);
-            }
-
-
-            foreach (var exit in exitToSource)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Path to exit:");
-
-                Node<T> current = exit;
-                List<Node<T>> path = new List<Node<T>>();
-
-                while (current != null)
-                {
-                    path.Add(current);
-                    current = parent[current];
-                }
-
-                path.Reverse();
-
-                foreach (var node in path)
-                {
-                    Console.WriteLine(node.Data);
-                }
-            }
-            Comparisons = marked.Count;
-            Console.WriteLine($"\nBFS total comparisons: {Comparisons}");
+          Console.WriteLine();
+          Console.WriteLine("Destination found!");
+          exitToSource.Add(node);
+          break;
         }
+        Console.Write(", ");
+        foreach (var edge in node.Edges)
+        {
+          if (!parent.ContainsKey(edge.To))
+          {
+            parent[edge.To] = node;
+            queue.Enqueue(edge.To);
+          }
+        }
+      }
+
+
+      foreach (var exit in exitToSource)
+      {
+        Node<T> current = exit;
+        List<Node<T>> path = new List<Node<T>>();
+
+        while (current != null)
+        {
+          path.Add(current);
+          current = parent[current];
+        }
+
+        path.Reverse();
+
+        Console.Write("Path to destination: ");
+        for (int i = 0; i < path.Count; i++)
+        {
+          Console.Write(path[i].Data);
+          if (i + 1 != path.Count)
+          {
+            Console.Write(" → ");
+          }
+        }
+      }
+      Comparisons = marked.Count;
+      Console.WriteLine($"\nBFS Visited Nodes: {Comparisons}");
     }
+  }
 }
